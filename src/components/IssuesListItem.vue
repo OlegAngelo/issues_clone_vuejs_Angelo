30f77544f23c6397a7fa527092cef58b439fdad5
Angelo <52464397+OlegAngelo@users.noreply.github.com>
2021-07-12 15:29:48 +0800

    [GIP-TASK] Issue Date Format (#3)
    * [GIP-TASK] Issue Date Format
    
    * [GIP-TASK] Issue Date Format (lessen code)
    
    Co-authored-by: balagtasangelo <ninoangelo.balagtas@sun-asterisk.com>

--- src/components/IssuesListItem.vue
+++ src/components/IssuesListItem.vue
@@ -8,19 +8,28 @@
 
       <br>
 
-      #{{ issue.number }} opened {{ issue.created_at }} by {{ issue.user.login }} 
+      #{{ issue.number }} opened {{ date_info }} by {{ issue.user.login }} 
     </div>
   </li>
 </template>
 
 <script>
-// import moment from 'moment';
+import moment from 'moment';
 
 export default {
   name: 'IssuesListItem',
   props: ['issue'],
-  methods: {
-    format_date() {
+  computed: {
+    date_info() {
+      var timestamp = moment(this.issue.created_at);
+      var current_month = moment().format("M");
+      var github_month = timestamp.format("M");
+
+      if (current_month === github_month) {
+        return timestamp.fromNow();
+      }
+        
+      return `on ${timestamp.format("MMM D")}`;
     }
   }
 }
