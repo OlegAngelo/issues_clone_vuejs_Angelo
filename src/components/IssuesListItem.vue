438907f350078c956132377c3b6ec9c3c696f3eb
Angelo <52464397+OlegAngelo@users.noreply.github.com>
2021-07-13 13:47:41 +0800

    [GIP-TASK] Display Issue Status and Labels (#4)
    * [GIP-TASK] Display Issue Status and Labels
    
    * [GIP-TASK] Display Issue Status and Labels
    
    Co-authored-by: balagtasangelo <ninoangelo.balagtas@sun-asterisk.com>

--- src/components/IssuesListItem.vue
+++ src/components/IssuesListItem.vue
@@ -1,9 +1,25 @@
 <template>
-  <li class="list-group-item media">
+  <li class="list-group-item media parent">
+    <div class="text-center mx-2">
+      <img 
+        v-if="issue.state === 'open'"
+        src="../assets/static/images/open.jpeg"
+      >
+      <img 
+        v-else
+        src="../assets/static/images/closed.jpeg" 
+      >
+    </div>
+
     <div class="media-body">
       <b>{{ issue.title }}</b>
-      <span>
-      {{ issue.labels.name }}
+      <span 
+        v-for="label in issue.labels"
+        :key="label.id"
+        :style="{ backgroundColor: `#${label.color}` }"
+        class="labels px-2 mx-2"
+      >
+        {{ label.name }}
       </span>
 
       <br>
@@ -36,4 +52,16 @@ export default {
 </script>
 
 <style scoped>
+.parent {
+    display: flex;
+}
+
+img {
+  width: 15px;
+  margin-right: 10px;
+}
+
+.labels {
+  border-radius: 12px;
+}
 </style>
